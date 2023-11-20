f634c930fce24fe1f4ab9ebda9ac2bd21b36b413
Hasibur <hasibur303@gmail.com>
2023-11-20 18:46:05 +0600

    cgpaCal

--- src/main/java/com/example/academichubuiu/HomePage.java
+++ src/main/java/com/example/academichubuiu/HomePage.java
@@ -1,4 +1,29 @@
 package com.example.academichubuiu;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class HomePage {
+    @FXML
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    @FXML
+    public void calculator(ActionEvent event) throws IOException
+    {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("cgpaCalculator.fxml"));
+        root = fxmlLoader.load();
+        scene = new Scene(root);
+
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
 }
