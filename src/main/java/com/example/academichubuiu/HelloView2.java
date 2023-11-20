34388a83ba7214587bdf37a15ffa3f1e6122af92
Hasibur <hasibur303@gmail.com>
2023-11-20 12:10:14 +0600

    updated

--- src/main/java/com/example/academichubuiu/HelloView2.java
+++ src/main/java/com/example/academichubuiu/HelloView2.java
@@ -1,4 +1,30 @@
 package com.example.academichubuiu;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class HelloView2 {
+    @FXML
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    @FXML
+    public void goTologinpage(ActionEvent event) throws IOException
+    {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        root = fxmlLoader.load();
+        scene = new Scene(root);
+
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
+
 }
