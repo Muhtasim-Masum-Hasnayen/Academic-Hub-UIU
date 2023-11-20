0a47809e267ff5dd677af362d06df2cb14d99e5f
Hasibur <hasibur303@gmail.com>
2023-11-20 14:12:11 +0600

    updated

--- src/main/java/com/example/academichubuiu/ForgetPassword.java
+++ src/main/java/com/example/academichubuiu/ForgetPassword.java
@@ -1,4 +1,29 @@
 package com.example.academichubuiu;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class ForgetPassword {
+    @FXML
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    @FXML
+    public void goTologinpage(ActionEvent event) throws IOException
+    {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("LoginPage.fxml"));
+        root = fxmlLoader.load();
+        scene = new Scene(root);
+
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
 }
