bcb117ffccc543be307bb9180401081ba3233f5d
Hasibur <hasibur303@gmail.com>
2023-12-04 22:52:34 +0600

    faculty

--- src/main/java/com/example/academichubuiu/LoginPage.java
+++ src/main/java/com/example/academichubuiu/LoginPage.java
@@ -10,6 +10,7 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
@@ -35,16 +36,21 @@ public class LoginPage implements Initializable {
     @FXML
     private Button play;
 
+    @FXML
+    ChoiceBox<String> mybox;
+    private String[] choice = {"Student", "Faculty"};
+
     @Override
-    public void initialize (URL url, ResourceBundle rb){
+    public void initialize(URL url, ResourceBundle rb) {
         play(null);
+        mybox.getItems().addAll(choice);
+        mybox.getSelectionModel().selectFirst();
     }
 
 
-
     @FXML
-    private void setRotate(Circle c,boolean reverse,int angle , int duration){
-        RotateTransition rt = new RotateTransition(Duration.seconds(duration),c);
+    private void setRotate(Circle c, boolean reverse, int angle, int duration) {
+        RotateTransition rt = new RotateTransition(Duration.seconds(duration), c);
         rt.setAutoReverse(reverse);
         rt.setByAngle(angle);
         rt.setDelay(Duration.seconds(0));
@@ -54,91 +60,100 @@ public class LoginPage implements Initializable {
     }
 
     @FXML
-    private void play(ActionEvent actionEvent){
-        setRotate(c1,true,360,12);
-        setRotate(c2,true,220,15);
-        setRotate(c3,true,145,20);
+    private void play(ActionEvent actionEvent) {
+        setRotate(c1, true, 360, 12);
+        setRotate(c2, true, 220, 15);
+        setRotate(c3, true, 145, 20);
 
     }
+
     @FXML
     private static Stage stage;
     private Scene scene;
     private Parent root;
 
     @FXML
-    public void goToRegistrationPage(MouseEvent event) throws IOException
-    {
+    public void goToRegistrationPage(MouseEvent event) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Registration.fxml"));
         root = fxmlLoader.load();
         scene = new Scene(root);
 
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         stage.setScene(scene);
         stage.show();
     }
-    public void goToForgetPassPage(MouseEvent event) throws IOException
-    {
+
+    public void goToForgetPassPage(MouseEvent event) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("ForgetPassword.fxml"));
         root = fxmlLoader.load();
         scene = new Scene(root);
 
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         stage.setScene(scene);
         stage.show();
     }
-    public void goToHomePage(ActionEvent event) throws IOException
-    {
+
+    public void goToHomePage(ActionEvent event) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("HomePage.fxml"));
         root = fxmlLoader.load();
         scene = new Scene(root);
 
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         stage.setScene(scene);
         stage.show();
     }
 
     @FXML
-    TextField studentid=new TextField();
+    TextField studentid = new TextField();
     @FXML
-    TextField password=new TextField();
+    TextField password = new TextField();
 
-@FXML
-public void loginStudent(ActionEvent event) {
-    String mstudentId = studentid.getText();
-    String mpassword = password.getText();
+    @FXML
+    public void loginStudent(ActionEvent event) {
+        if (mybox.getValue().equals("Student")){
+            String mstudentId = studentid.getText();
+        String mpassword = password.getText();
 
-    boolean loginSuccessful = false;
+        boolean loginSuccessful = false;
 
-    try (BufferedReader reader = new BufferedReader(new FileReader("student_data.txt"))) {
-        String line;
+        try (BufferedReader reader = new BufferedReader(new FileReader("student_data.txt"))) {
+            String line;
 
-        // Read each line from the file
-        while ((line = reader.readLine()) != null) {
-            String[] parts = line.split(" ");
-            String studentId = parts[0];
-            String emailAddress = parts[1];
-            String password = parts[2];
+            // Read each line from the file
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(" ");
+                String studentId = parts[0];
+                String emailAddress = parts[1];
+                String password = parts[2];
 
-            // Check if provided credentials match any record in the file
-            if (mstudentId.equals(studentId) && mpassword.equals(password)) {
-                loginSuccessful = true;
-                break;
+                // Check if provided credentials match any record in the file
+                if (mstudentId.equals(studentId) && mpassword.equals(password)) {
+                    loginSuccessful = true;
+                    break;
+                }
             }
-        }
 
-    } catch (IOException e) {
-        System.err.println("Error reading from file: " + e.getMessage());
-    }
-
-    if (loginSuccessful) {
-        try {
-            goToHomePage(event);
         } catch (IOException e) {
-            e.printStackTrace(); // Handle the exception according to your needs
+            System.err.println("Error reading from file: " + e.getMessage());
         }
-    } else {
-        showMessage("Invalid Information. Login failed.", "Login");
+
+        if (loginSuccessful) {
+            try {
+                goToHomePage(event);
+            } catch (IOException e) {
+                e.printStackTrace(); // Handle the exception according to your needs
+            }
+        } else {
+            showMessage("Invalid Information. Login failed.", "Login");
+        }
     }
+        else{
+
+            showMessage("Invalid Faculty Information. Login failed.", "Login");
+
+        }
+
+
 }
 
 
--- src/main/java/com/example/academichubuiu/taskcontroller.java
+++ src/main/java/com/example/academichubuiu/taskcontroller.java
@@ -9,15 +9,11 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
-import javafx.stage.Stage;
 import javafx.util.Callback;
 
 import java.io.IOException;
@@ -222,22 +218,5 @@ public class taskcontroller {
     @FXML
     public void handleExit(){
         Platform.exit();
-    }
-    @FXML
-    private Stage stage;
-    @FXML
-    private Scene scene;
-    @FXML
-    private Parent root;
-    @FXML
-    public void goTohomepage(ActionEvent event) throws IOException
-    {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("HomePage.fxml"));
-        root = fxmlLoader.load();
-        scene = new Scene(root);
-
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        stage.setScene(scene);
-        stage.show();
     }
 }
