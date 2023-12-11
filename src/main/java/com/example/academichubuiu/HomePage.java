eda6c25c4302110045b7e4615e3fd2e5f7101b57
USER <hasnayenmasum@gmail.com>
2023-12-11 19:30:50 +0600

    Design

--- src/main/java/com/example/academichubuiu/HomePage.java
+++ src/main/java/com/example/academichubuiu/HomePage.java
@@ -1,18 +1,29 @@
 package com.example.academichubuiu;
 
+import java.net.URL;
+
+import javafx.animation.FadeTransition;
+import javafx.animation.Interpolator;
+import javafx.animation.RotateTransition;
+import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.scene.transform.Rotate;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.io.IOException;
+import java.util.ResourceBundle;
 
 import static com.example.academichubuiu.LoginPage.showMessage;
 
-public class HomePage {
+public class HomePage implements Initializable{
     @FXML
     private Stage stage;
     @FXML
@@ -84,5 +95,31 @@ public class HomePage {
         stage.show();
         showMessage("Successfully Logout", "Logout");
     }
+    @FXML
+    private ImageView myImage;
+    @FXML
+    private ImageView bookImage;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        RotateTransition rotate = new RotateTransition();
+        rotate.setNode(myImage);
+        rotate.setDuration(Duration.millis(6000));
+        rotate.setCycleCount(TranslateTransition.INDEFINITE);
+        rotate.setInterpolator(Interpolator.LINEAR);
+        rotate.setByAngle(360);
+        rotate.setAxis(Rotate.Y_AXIS);
+        rotate.play();
+
+        FadeTransition fade = new FadeTransition();
+        fade.setNode(bookImage);
+        fade.setDuration(Duration.millis(5000));
+        fade.setCycleCount(TranslateTransition.INDEFINITE);
+        fade.setInterpolator(Interpolator.LINEAR);
+        fade.setFromValue(0);
+        fade.setToValue(1);
+        fade.play();
+    }
+
 }
 
