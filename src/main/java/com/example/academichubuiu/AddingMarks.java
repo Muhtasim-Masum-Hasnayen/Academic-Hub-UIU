1cbde328ea182cfffc92f083d1430f05eb021add
USER <hasnayenmasum@gmail.com>
2023-12-13 22:21:41 +0600

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	src/main/java/com/example/academichubuiu/Controller.java

--- src/main/java/com/example/academichubuiu/AddingMarks.java
+++ src/main/java/com/example/academichubuiu/AddingMarks.java
@@ -1,18 +1,28 @@
 package com.example.academichubuiu;
 
+import javafx.animation.FadeTransition;
+import javafx.animation.Interpolator;
+import javafx.animation.RotateTransition;
+import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.scene.transform.Rotate;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 import static com.example.academichubuiu.LoginPage.showMessage;
 
-public class AddingMarks {
+public class AddingMarks implements Initializable {
     @FXML
     private Stage stage;
     @FXML
@@ -40,6 +50,36 @@ public class AddingMarks {
         stage.setScene(scene);
         stage.show();
     }
+
+
+
+    @FXML
+    private ImageView myImage;
+    @FXML
+    private ImageView bookImage;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        RotateTransition rotate = new RotateTransition();
+        rotate.setNode(myImage);
+        rotate.setDuration(Duration.millis(6000));
+        rotate.setCycleCount(TranslateTransition.INDEFINITE);
+        rotate.setInterpolator(Interpolator.LINEAR);
+        rotate.setByAngle(360);
+        rotate.setAxis(Rotate.Y_AXIS);
+        rotate.play();
+
+        FadeTransition fade = new FadeTransition();
+        fade.setNode(bookImage);
+        fade.setDuration(Duration.millis(5000));
+        fade.setCycleCount(TranslateTransition.INDEFINITE);
+        fade.setInterpolator(Interpolator.LINEAR);
+        fade.setFromValue(0);
+        fade.setToValue(1);
+        fade.play();
+    }
+
+
     public void gotoThome(ActionEvent event) throws IOException
     {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("teacher.fxml"));
